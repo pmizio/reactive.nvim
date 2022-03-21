2dbd16d4f5b518d908326152b3933803c0eabf5f
Pawel Mizio <pawel.mizio@allegro.pl>
2022-03-21 18:55:39 +0100

    feat: clean

--- lua/config/lsp/format.lua
+++ lua/config/lsp/format.lua
@@ -1,13 +1,13 @@
 local lsp_format = require "lsp-format"
 local null_ls = require "null-ls"
 
-local js_ts_order = { "eslint", "null-ls" }
+local TS_ORDER = { "eslint", "null-ls" }
 
 lsp_format.setup {
-  typescript = { tab_width = 2, order = js_ts_order },
-  typescriptreact = { tab_width = 2, order = js_ts_order },
-  javascript = { tab_width = 2, order = js_ts_order },
-  javascriptreact = { tab_width = 2, order = js_ts_order },
+  typescript = { tab_width = 2, order = TS_ORDER },
+  typescriptreact = { tab_width = 2, order = TS_ORDER },
+  javascript = { tab_width = 2, order = TS_ORDER },
+  javascriptreact = { tab_width = 2, order = TS_ORDER },
   lua = { tab_width = 2 },
   rust = { tab_width = 4 },
 }
@@ -20,22 +20,3 @@ null_ls.setup {
     null_ls.builtins.formatting.rustfmt,
   },
 }
-
--- require("nvim-lsp-installer").on_server_ready(function(server)
---   if server.name == "efm" then
---     server:setup {
---       on_attach = require("lsp-format").on_attach,
---       init_options = { documentFormatting = true },
---       settings = {
---         languages = {
---           typescript = { prettier },
---           typescriptreact = { prettier },
---           javascript = { prettier },
---           javascriptreact = { prettier },
---           lua = { stylua },
---           rust = { rustfmt },
---         },
---       },
---     }
---   end
--- end)
