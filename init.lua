dd246dfebb8183b15ad693df4b9e9c92ab57908c
Pawel Mizio <pawel.mizio@allegro.pl>
2022-06-09 11:12:30 +0200

    chore: move commit autocmd to fugitive config

--- init.lua
+++ init.lua
@@ -315,13 +315,12 @@ one_au("TextYankPost", {
 one_au("FileType", {
   pattern = "gitcommit",
   callback = function()
+    local branch = fn.system("git branch --show-current"):match "/?([%u%d]+-%d+)-?"
     local content = vim.api.nvim_buf_get_lines(0, 0, -1, false)[1]
 
-    if content:find "^Merge branch" == nil then
+    if branch and content:find "^Merge branch" == nil then
       return
     end
-
-    local branch = fn.system("git branch --show-current"):match "/?([%u%d]+-%d+)-?"
 
     if branch then
       vim.api.nvim_buf_set_lines(0, 0, -1, false, { branch .. " | " })
--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,5 +1,6 @@
 local map = vim.keymap.set
 local fn = vim.fn
+local one_au = require("config.utils").create_onetime_autocmd
 
 -- TODO: convert viml to lua
 local function toggle_fugitive_status()
@@ -11,3 +12,21 @@ local function toggle_fugitive_status()
 end
 
 map("n", "<leader>gs", toggle_fugitive_status)
+
+one_au("FileType", {
+  pattern = "gitcommit",
+  callback = function()
+    local content = vim.api.nvim_buf_get_lines(0, 0, -1, false)[1]
+
+    if content ~= "" and content:find "^Merge branch" == nil then
+      return
+    end
+
+    local branch = fn.system("git branch --show-current"):match "/?([%u%d]+-%d+)-?"
+
+    if branch then
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, { branch .. " | " })
+      vim.cmd ":startinsert!"
+    end
+  end,
+})
