6dae78dc9224d781084130dea5fb9200e4518afb
Pawel Mizio <pawel.mizio@allegro.pl>
2022-06-30 09:26:04 +0200

    chore: rename mapping utils

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,40 +1,44 @@
 local fn = vim.fn
 local m = require "config.utils.map"
 
-m.nnoremap("<ESC>", ":nohl <ESC>")
+m.nmap("<ESC>", ":nohl <ESC>")
 
 -- map jk and kj to esc
-m.inoremap("<ESC>", "<NOP>")
-m.inoremap("jk", "<C-o>:nohl<CR><ESC>")
-m.inoremap("kj", "<C-o>:nohl<CR><ESC>")
-m.tnoremap("jk", "<C-\\><C-n>")
-m.tnoremap("kj", "<C-\\><C-n>")
+m.imap("<ESC>", "<NOP>")
+m.imap("jk", "<C-o>:nohl<CR><ESC>")
+m.imap("kj", "<C-o>:nohl<CR><ESC>")
+m.tmap("jk", "<C-\\><C-n>")
+m.tmap("kj", "<C-\\><C-n>")
 
 -- disable ex mode
-m.noremap("", "Q", "<NOP>")
+m.map("", "Q", "<NOP>")
 
 -- autocomplete remappings
-m.map({ "i", "c" }, "<C-j>", "<C-n>")
-m.map({ "i", "c" }, "<C-k>", "<C-p>")
+m.remap({ "i", "c" }, "<C-j>", "<C-n>")
+m.remap({ "i", "c" }, "<C-k>", "<C-p>")
 
 -- map keys for move between splits
-m.nnoremap("<C-j>", "<C-w><C-j>")
-m.nnoremap("<C-k>", "<C-w><C-k>")
-m.nnoremap("<C-l>", "<C-w><C-l>")
-m.nnoremap("<C-h>", "<C-w><C-h>")
+m.nmap("<C-j>", "<C-w><C-j>")
+m.nmap("<C-k>", "<C-w><C-k>")
+m.nmap("<C-l>", "<C-w><C-l>")
+m.nmap("<C-h>", "<C-w><C-h>")
 
 -- map keys for moving lines up and down
 local Aj = fn.has "macunix" == 1 and "∆" or "<A-j>"
 local Ak = fn.has "macunix" == 1 and "Ż" or "<A-k>"
-m.nnoremap(Aj, ":m .+1<CR>==")
-m.nnoremap(Ak, ":m .-2<CR>==")
-m.vnoremap(Aj, ":m '>+1<CR>gv=gv")
-m.vnoremap(Ak, ":m '<-2<CR>gv=gv")
+m.nmap(Aj, ":m .+1<CR>==")
+m.nmap(Ak, ":m .-2<CR>==")
+m.vmap(Aj, ":m '>+1<CR>gv=gv")
+m.vmap(Ak, ":m '<-2<CR>gv=gv")
+
+-- fix indenting using < and >
+m.vmap(">", ">gv")
+m.vmap("<", "<gv")
 
 -- map keys for yank and paste over system clipboard
-m.noremap({ "n", "v" }, "gp", '"+p')
-m.noremap({ "n", "v" }, "gP", '"+P')
+m.map({ "n", "v" }, "gp", '"+p')
+m.map({ "n", "v" }, "gP", '"+P')
 
 -- yank
-m.noremap({ "n", "v" }, "gy", '"+y')
-m.noremap({ "n", "v" }, "gY", '"+Y')
+m.map({ "n", "v" }, "gy", '"+y')
+m.map({ "n", "v" }, "gY", '"+Y')
---
+++ lua/config/utils/map.lua
@@ -0,0 +1,29 @@
+local map = vim.keymap.set
+
+local M = {}
+
+function M.map(mode, lhs, rhs, options)
+  map(mode, lhs, rhs, vim.tbl_extend("force", options or {}, { silent = true }))
+end
+
+function M.remap(mode, lhs, rhs, options)
+  M.map(mode, lhs, rhs, vim.tbl_extend("force", options or {}, { remap = true }))
+end
+
+function M.nmap(...)
+  M.map("n", ...)
+end
+
+function M.imap(...)
+  M.map("i", ...)
+end
+
+function M.vmap(...)
+  M.map("v", ...)
+end
+
+function M.tmap(...)
+  M.map("t", ...)
+end
+
+return M
