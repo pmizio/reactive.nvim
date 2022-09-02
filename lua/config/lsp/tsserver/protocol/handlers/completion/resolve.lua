1ac7016a82bd1df6b579e5b9488bcedfcf5fe178
Paweł Mizio <mizio.pawel@gmail.com>
2022-09-02 18:48:18 +0200

    feat(tsserver plugin): full `completionItem/resolve` implementation

--- lua/config/lsp/tsserver/protocol/handlers/completion/resolve.lua
+++ lua/config/lsp/tsserver/protocol/handlers/completion/resolve.lua
@@ -1,5 +1,4 @@
 local constants = require "config.lsp.tsserver.protocol.constants"
-local item_kind_utils = require "config.lsp.tsserver.protocol.handlers.completion.item_kind_utils"
 local utils = require "config.lsp.tsserver.protocol.utils"
 
 -- tsserver protocol reference
@@ -24,30 +23,53 @@ local completion_request_handler = function(_, params)
   }
 end
 
-local concat_description = function(parts, delim)
+local concat_description = function(parts, delim, tag_formatting)
   delim = delim or ""
   return table.concat(vim.tbl_map(function(it)
+    if tag_formatting and it.kind == "parameterName" then
+      return "`" .. it.text .. "`"
+    end
+
     return it.text
   end, parts) or {}, delim)
 end
 
 local make_tags = function(tags)
   return table.concat(vim.tbl_map(function(it)
-    local parts = { "\n\n_@" }
+    local parts = { "\n_@" }
     table.insert(parts, it.name)
-    table.insert(parts, "_ — `")
-    table.insert(parts, concat_description(it.text))
-    table.insert(parts, "`")
+    table.insert(parts, "_ — ")
+    table.insert(parts, concat_description(it.text, nil, true))
 
     return table.concat(parts, "")
   end, tags) or {}, "\n")
+end
+
+local make_text_edits = function(code_actions)
+  if not code_actions then
+    return nil
+  end
+
+  local text_edits = {}
+
+  for _, action in ipairs(code_actions) do
+    for _, changes_set in ipairs(action.changes) do
+      for _, change in ipairs(changes_set.textChanges) do
+        table.insert(text_edits, {
+          newText = change.newText,
+          range = utils.convert_tsserver_range_to_lsp(change),
+        })
+      end
+    end
+  end
+
+  return text_edits
 end
 
 local completion_response_handler = function(_, body, request_params)
   if body and body[1] then
     local details = body[1]
 
-    P(details.tags)
     table.insert(details.documentation, { text = make_tags(details.tags) })
     return vim.tbl_extend("force", request_params, {
       detail = concat_description(details.displayParts),
@@ -55,6 +77,9 @@ local completion_response_handler = function(_, body, request_params)
         kind = constants.MarkupKind.Markdown,
         value = concat_description(details.documentation, "\n"),
       },
+      additionalTextEdits = make_text_edits(details.codeActions),
+      -- INFO: there is also `command` prop but I don't know there is usecase for that here,
+      -- or neovim even handle than for now i skip this
     })
   end
 
